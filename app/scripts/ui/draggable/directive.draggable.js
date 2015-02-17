40bdd47150d7fb74302a9a5f221bc19b83ca473f
Marcin Krawczyk <marcinn.krawczyk@gmail.com>
2015-02-18 00:25:11 +0100

    changes with z-index

--- app/scripts/ui/draggable/directive.draggable.js
+++ app/scripts/ui/draggable/directive.draggable.js
@@ -1,8 +1,21 @@
-window.modules.ui.draggable = function () {
+window.modules.ui.draggable = function (Storage, zIndex) {
   return {
     restrict: 'A',
-    link: function (scope, element) {
-      $(element).draggable();
+    link: function (scope, element, attr) {
+      element.draggable({
+        start: function () {
+          element.zIndex(zIndex++);
+        },
+        stop: function () {
+          Storage.save(attr.draggable, element.position());
+        }
+      });
+
+      var previousPosition = Storage.get(attr.draggable);
+
+      if (previousPosition) {
+        element.offset(previousPosition);
+      }
     }
   }
 };
--- app/scripts/ui/module.js
+++ app/scripts/ui/module.js
@@ -2,6 +2,7 @@
 
 (function (window, angular) {
   angular
-    .module('wintubeApp.ui', [])
-    .directive('draggable', window.modules.ui.draggable)
+    .module('wintubeApp.ui', ['wintubeApp.storage'])
+    .value("zIndex", 0)
+    .directive('draggable', ['Storage', 'zIndex', window.modules.ui.draggable])
 }(window, window.angular));
